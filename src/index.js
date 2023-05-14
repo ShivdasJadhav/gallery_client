9d4666f663cfdc0e26f27bee57b3952008b40f44
ShivdasJadhav <jshivdas07@gmail.com>
2023-05-14 16:11:16 +0530

    error detected to link tailwind output.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Auther from './Auther';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./assets/css/output.css";
+import reportWebVitals from "./reportWebVitals";
+import Auther from "./Auther";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auther />
