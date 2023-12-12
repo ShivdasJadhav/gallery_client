6d67cdb9f49a1814ae30c1e1f54bc72342be7cc2
ShivdasJadhav <jshivdas07@gmail.com>
2023-12-12 21:48:02 +0530

    image resource for the application updated

--- src/Auth/About.jsx
+++ src/Auth/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import bg_logo from "../assets/img/bg_logo.png";
-import logo from "../assets/img/logo.png";
+import { bg_logo, logo } from "../assets/img";
 function About() {
   return (
     <div id="about" className="my-4 py-8">
@@ -9,24 +8,29 @@ function About() {
       </h2>
       <div className="relative w-10/12 text-fjord_one text-sm text-justify mx-auto md:text-lg flex">
         <div className="flex-1 md:px-14">
-        <img src={bg_logo} alt="" className="absolute w-10/12 h-auto top-10 left-6 md:hidden" />
-        <p className="my-5">
-          “Art Exhibits”, where creativity and innovation collide in the digital
-          realm. We are passionate about art and the immense talent that exists
-          within the artistic community. Our platform was born out of the desire
-          to create a space where artists can flourish, and organizations can
-          discover the brilliance that lies within the world of art.
-        </p>
-        <p className="my-5">
-          here, our mission is to provide artists with a platform to showcase
-          their artistic prowess, connect with fellow creatives, and receive
-          valuable feedback on their work. We also aim to empower organizations,
-          art enthusiasts, and talent seekers to find the perfect artistic
-          expressions that resonate with their vision.
-        </p>
+          <img
+            src={bg_logo}
+            alt=""
+            className="absolute w-10/12 h-auto top-10 left-6 md:hidden"
+          />
+          <p className="my-5">
+            “Art Exhibits”, where creativity and innovation collide in the
+            digital realm. We are passionate about art and the immense talent
+            that exists within the artistic community. Our platform was born out
+            of the desire to create a space where artists can flourish, and
+            organizations can discover the brilliance that lies within the world
+            of art.
+          </p>
+          <p className="my-5">
+            here, our mission is to provide artists with a platform to showcase
+            their artistic prowess, connect with fellow creatives, and receive
+            valuable feedback on their work. We also aim to empower
+            organizations, art enthusiasts, and talent seekers to find the
+            perfect artistic expressions that resonate with their vision.
+          </p>
         </div>
         <div className="hidden flex-1 md:flex  items-center">
-        <img src={logo} alt="" className="w-54 h-auto mx-auto" />
+          <img src={logo} alt="" className="w-54 h-auto mx-auto" />
         </div>
       </div>
     </div>
---
+++ src/assets/img/index.js
@@ -0,0 +1,37 @@
+import bg_logo from "./bg_logo.png";
+import close_menu from "./close_menu.png";
+import icon_del from "./delete.png";
+import icon_edit from "./icon_edit.png";
+import forgot from "./forgot.png";
+import icon_like from "./like.png";
+import logo from "./logo.png";
+import open_menu from "./open_menu.png";
+import icon_order from "./icon_order.png";
+import published from "./published.png";
+import icon_search from "./icon_search.png";
+import rejected from "./rejected.png";
+import reviewed from "./reviewed.png";
+import icon_user from "./icon_user.png";
+import icon_upload from "./icon_upload.png";
+import icon_view from "./icon_view.png";
+import view_more from "./view_more.png";
+
+export {
+  bg_logo,
+  close_menu,
+  icon_del,
+  icon_edit,
+  forgot,
+  icon_like,
+  logo,
+  open_menu,
+  icon_order,
+  published,
+  icon_search,
+  rejected,
+  reviewed,
+  icon_user,
+  icon_upload,
+  icon_view,
+  view_more,
+};
