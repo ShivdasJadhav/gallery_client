344d3581936c18d5dc0cece849dc40f65165a37b
ShivdasJadhav <jshivdas07@gmail.com>
2023-12-16 18:52:43 +0530

    updated as per new requirements for dashboard on proposal approval

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./comp/Navbar";
-import Update from "./comp/Update";
 import Home from "./comp/Home";
-import About from "./comp/About";
 import Profile from "./comp/Profile";
 import Proposals from "./comp/Proposals";
-import Add from "./comp/Add";
-import Profile_view from "./comp/Profile_view.jsx";
-import Admin_proposals from "./comp/Admin_proposals";
-import { useState } from "react";
 import Dashboard from "./comp/Dashboard";
 import Art_pice from "./comp/Art_pice.jsx";
 import { Toaster } from "react-hot-toast";
 function App(props) {
-  let user=JSON.parse(localStorage.getItem("user"))
+  let user = JSON.parse(localStorage.getItem("user"));
   return (
     <>
       <BrowserRouter>
@@ -22,10 +16,13 @@ function App(props) {
           <Toaster />
           <Navbar />
           <Routes>
-            <Route path="/*" element={<Home user={user}/>} />
-            <Route path="/proposal" element={<Proposals user={user}/>} />
-            <Route path="/art" element={<Art_pice user={user}/>} />
-            <Route path="/profile" element={<Profile user={user}/>} />
+            <Route path="/*" element={<Home user={user} />} />
+            <Route path="/proposal" element={<Proposals user={user} />} />
+            <Route path="/art/:id" element={<Art_pice user={user} />} />
+            <Route path="/profile" element={<Profile user={user} />} />
+            {user.isAdmin && (
+              <Route path="/dashboard" element={<Dashboard user={user} />} />
+            )}
           </Routes>
         </div>
       </BrowserRouter>
