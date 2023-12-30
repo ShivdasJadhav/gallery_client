858a364f9027e0c1e6a48d41371ad448011235b4
ShivdasJadhav <jshivdas07@gmail.com>
2023-12-30 11:51:15 +0530

    optimised image resource loading

--- src/comp/Art.jsx
+++ src/comp/Art.jsx
@@ -22,11 +22,11 @@ function Art(props) {
   };
   return (
     <div
-      style={{ backgroundImage: `url('${props.data.img}')` }}
-      className={`h-64 my-4 md:my-2 md:m-0 relative border border-sky-600 rounded-xl  bg-no-repeat bg-center bg-cover `}
+      className={`h-64 relative my-4 md:my-2 md:m-0 relative border border-sky-600 rounded-xl`}
     >
+      <img src={props.data.img} alt={props.data._id} className="absolute w-full h-full z-0 rounded-xl" />
       {props.isProfile && (
-        <div className="absolute flex right-1 top-[-12px] block">
+        <div className="absolute z-10 flex right-1 top-[-12px] block">
           <button
             onClick={() => props.handlePopup("update", "block", props.data)}
           >
@@ -52,7 +52,7 @@ function Art(props) {
             navigate(`/art/${props.data._id}`);
           }
         }}
-        className="mt-auto rounded-xl border border-fuchsia-600 border-x-0 border-b-0  bg-white hover:cursor-pointer absolute bottom-0 h-32 w-full"
+        className="mt-auto z-10 rounded-xl border border-fuchsia-600 border-x-0 border-b-0  bg-white hover:cursor-pointer absolute bottom-0 h-32 w-full"
       >
         <h3 className="text-fuchsia-900 text-allura text-base text-center my-1">
           {props.data.title}
